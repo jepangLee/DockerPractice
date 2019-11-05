0946dc9f04d925c882128ac074c53ac2be0d97d1
JepangLee <ljsung0805@gmail.com>
2019-11-05 23:46:35 +0900

    [EDTED] jar to war

---
+++ src/main/java/com/Controller.java
@@ -0,0 +1,12 @@
+package com;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @RequestMapping("/")
+    public String home() {
+        return "Hello Docker World";
+    }
+}
