ff95b0eca73e25c85c94e1151e728ca931373dd3
JepangLee <ljsung0805@gmail.com>
2019-11-06 11:28:26 +0900

    [ADDED] tomcat setting

--- src/main/java/com/Controller.java
+++ src/main/java/com/Controller.java
@@ -1,11 +1,11 @@
 package com;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
-    @RequestMapping("/")
+    @GetMapping("/")
     public String home() {
         return "Hello Docker World";
     }
